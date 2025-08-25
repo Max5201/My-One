6d1c5c93bee8c691e0860e6a92df2f5c95500bbf
Max5201 <maxone520231@gmail.com>
2025-08-25 10:28:25 -0700

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,5 @@
 async function getMessage() {
-  const res = await fetch('https://your-backend-name.onrender.com/message');
+  const res = await fetch('https://my-one-fxkn.onrender.com/message');
   const data = await res.json();
-  document.getElementById('response').innerText = `${data.text} (${data.created_at})`;
+  document.getElementById('response').innerText = data.message || data.text;
 }
