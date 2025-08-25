85f8b638995ca5c5a563de37652131e5807392de
Max5201 <maxone520231@gmail.com>
2025-08-25 09:06:30 -0700

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,5 @@
-
+async function getMessage() {
+  const res = await fetch('https://你的后端域名/message');
+  const data = await res.json();
+  document.getElementById('response').innerText = data.message;
+}
